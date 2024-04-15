1c4a47048e40c6ce70020d99f2d5fe2e227afd12
Nick Rabone <n.rabone72@aol.com>
2024-04-15 21:26:38 +0100

    Updated - set up creation of users, authentication with the Database.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -44,4 +44,25 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapshot = await getDoc(userDocRef)
   console.log(userSnapshot)
   console.log(userSnapshot.exists())
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch (error) {
+      console.log('Error creating user', error.message)
+    }
+  }
+  return userDocRef
 } 
+
+
+
+
+
+
