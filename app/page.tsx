f86b6a19702e9289dcd66ab29e22a3322c69d639
Nimira43 <n.rabone72@aol.com>
2024-11-03 11:05:39 +0000

    Shadcn - testing: ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 function HomePage() {
   return (
-    <h1 className="text-3xl">Home Page</h1>
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant='outline' size='lg' className='capitalize m-8'>
+        Submit
+      </Button>
+    </div>
+    
   )
 }
 export default HomePage
