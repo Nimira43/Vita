9415cc3ef6b19f8ab21199b34b7ec57d2a7b6de9
Nimira43 <n.rabone72@aol.com>
2024-11-03 13:55:19 +0000

    Brought in Shopping Cart to Navbar. Also changed the Shadcn theme

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,6 +1,23 @@
-function CartButton() {
+import { Button } from '../ui/button'
+import { PiShoppingCartLight } from 'react-icons/pi'
+import Link from 'next/link'
+
+async function CartButton() {
+  const numItemsInCart = 9
   return (
-    <div>CartButton</div>
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='flex justify-center items-center relative'
+    >
+      <Link href='/cart'>
+        <PiShoppingCartLight />
+        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-content text-xs'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
   )
 }
 export default CartButton
