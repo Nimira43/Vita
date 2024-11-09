8194c8ac529e5c3108ab5ec7b484cb59c64c030d
Nimira43 <n.rabone72@aol.com>
2024-11-09 20:21:06 +0000

    After testing Prisma and Supabase now writing schema for project database

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,8 @@
-import db from '@/utils/db'
-
-async function AboutPage() {
-  const profile = await db.testProfile.create({
-    data: {
-      name: 'settee',
-    },
-  })
-
-  const users = await db.testProfile.findMany()
-
+function AboutPage() {
+  
   return (
     <div>
-      {users.map((user) => {
-        return (
-          <h2 key={user.id} className='text-2xl font-bold'>
-            {user.name}
-          </h2>
-        )
-      })}
+      <h1>About Page</h1>
     </div>
   )
 }
