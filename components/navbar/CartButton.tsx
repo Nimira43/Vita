ac2b99f0e6d4ae72128ba49d5040606d2c3b6d55
Nimira43 <n.rabone72@aol.com>
2025-01-25 23:48:10 +0000

    Cart button brought back

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,23 +1,23 @@
-// import { Button } from '@/components/ui/button'
-// import { PiShoppingCartLight } from 'react-icons/pi'
-// import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { PiShoppingCartLight } from 'react-icons/pi'
+import Link from 'next/link'
 
-// async function CartButton() {
-//   const numItemsInCart = 9
-//   return (
-//     <Button
-//       asChild
-//       variant='outline'
-//       size='icon'
-//       className='flex justify-center items-center relative'
-//     >
-//       <Link href='/cart'>
-//         <PiShoppingCartLight />
-//         <span className='absolute -top-3 -right-3 bg-primary text-[#111] rounded-full h-6 w-6 flex items-center justify-center text-xs'>
-//           {numItemsInCart}
-//         </span>
-//       </Link>
-//     </Button>
-//   )
-// }
-// export default CartButton
+async function CartButton() {
+  const numItemsInCart = 9
+  return (
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='flex justify-center items-center relative'
+    >
+      <Link href='/cart'>
+        <PiShoppingCartLight />
+        <span className='absolute -top-3 -right-3 bg-primary text-[#111] rounded-full h-6 w-6 flex items-center justify-center text-xs'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  )
+}
+export default CartButton
