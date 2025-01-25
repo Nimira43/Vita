a202fc1bebf70a34589c0a4bb8d9c97af06c734b
Nimira43 <n.rabone72@aol.com>
2025-01-25 23:45:39 +0000

    Logo placeholder brought in

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,27 +1,14 @@
-import React from 'react'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import { RiEthFill } from 'react-icons/ri'
 
-const Logo = () => {
+function Logo() {
   return (
-    <div>
-      <h1>Logo</h1>
-    </div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <RiEthFill className='w-6 h-6 text-[#111]' />
+      </Link>
+    </Button>
   )
 }
-
 export default Logo
-
-
-// import Link from 'next/link'
-// import { Button } from '../ui/button'
-// import { RiEthFill } from 'react-icons/ri'
-
-// function Logo() {
-//   return (
-//     <Button size='icon' asChild>
-//       <Link href='/'>
-//         <RiEthFill className='w-6 h-6 text-[#111]' />
-//       </Link>
-//     </Button>
-//   )
-// }
-// export default Logo
