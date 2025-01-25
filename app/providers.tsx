cf6470053678be3b342782d181ce4120c914446b
Nimira43 <n.rabone72@aol.com>
2025-01-25 23:59:36 +0000

    Created  ThemePRovoder and brought into providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,20 @@
 'use client'
 
+import { ThemeProvider } from "./theme-provider"
+
 function Providers({ children }:   { children: React.ReactNode }) {
   return (
-    <>
-      {children}
-    </>
-    // <ThemeProvider
-    //   attribute='class'
-    //   defaultTheme='system'
-    //   enableSystem
-    //   disableTransitionOnChange
-    // >
+    // <>
     //   {children}
-    // </ThemeProvider>
+    // </>
+    <ThemeProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
   )
 }
 export default Providers
