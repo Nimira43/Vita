d37c7b6e79775f2b05581b6f3868811712213365
Nimira43 <n.rabone72@aol.com>
2025-01-25 23:29:39 +0000

    Brought nack the code in globals/Container.tsx and reviewed

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -1,28 +1,17 @@
-const Container = () => {
+import { cn } from '@/lib/utils'
+
+function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
   return (
-    <div>
-      <h1>Container</h1>      
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+      {children}
     </div>
   )
 }
 
 export default Container
-
-
-
-// import { cn } from '@/lib/utils'
-
-// function Container({
-//   children,
-//   className,
-// }: {
-//   children: React.ReactNode
-//   className?: string
-// }) {
-//   return (
-//     <div className={cn('mx-auto max-w-6xl xl:max-w-7xl p-8', className)}>
-//       {children}
-//     </div>
-//   )
-// }
-// export default Container
