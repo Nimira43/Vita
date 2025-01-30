d77bf6eae6c6635ae183d2ff9637cc90db30cfde
Nimira43 <n.rabone72@aol.com>
2025-01-30 20:54:16 +0000

    Brought back code for LinksDropDown.tsx

--- components/navbar/LinksDropDown.tsx
+++ components/navbar/LinksDropDown.tsx
@@ -1,42 +1,29 @@
-import React from 'react'
+import { RiMenu5Line } from 'react-icons/ri'
+import { Button } from '../ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import { links } from '@/utils/links'
+import Link from 'next/link'
 
-const LinksDropDown = () => {
+function LinksDropDown() {
   return (
-    <div>
-      <h1>LinkDropdown</h1>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+          <RiMenu5Line  className='w-6 h-6' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='capitalize w-full'>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
-
 export default LinksDropDown
-
-
-// import { RiMenu5Line } from 'react-icons/ri'
-// import { Button } from '../ui/button'
-// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu'
-// import { links } from '@/utils/links'
-// import Link from 'next/link'
-
-// function LinksDropDown() {
-//   return (
-//     <DropdownMenu>
-//       <DropdownMenuTrigger asChild>
-//         <Button variant='outline' className='flex gap-4 max-w-[100px]'>
-//           <RiMenu5Line  className='w-6 h-6' />
-//         </Button>
-//       </DropdownMenuTrigger>
-//       <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
-//         {links.map((link) => {
-//           return (
-//             <DropdownMenuItem key={link.href}>
-//               <Link href={link.href} className='capitalize w-full'>
-//                 {link.label}
-//               </Link>
-//             </DropdownMenuItem>
-//           )
-//         })}
-//       </DropdownMenuContent>
-//     </DropdownMenu>
-//   )
-// }
-// export default LinksDropDown
