73608b627eea875ff79631e669b4879e77b35b09
Nimira43 <n.rabone72@aol.com>
2025-03-29 09:20:18 +0000

    Clean up and correcting favourites route typos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,7 @@
-import db from '@/utils/db'
-
 async function AboutPage() {
-  const profile = await db.testProfile.create({
-    data: {
-      name: 'Test'
-    }
-  })
-const users = await db.testProfile.findMany()
   return (
     <div>
-      {users.map((user) => {
-        return (
-          <h2 key={user.id} className='text-2xl'
-          >
-            {user.name}
-          </h2>
-        )
-      })}
+      <h1>About</h1>
     </div>
   )
 }
