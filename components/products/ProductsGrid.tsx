ca8da019544292efa6917082d31a824be296d3b1
Nimira43 <n.rabone72@aol.com>
2025-04-12 17:07:31 +0100

    Bringing products in in ProductsGrid.

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -3,6 +3,7 @@ import { Product } from '@prisma/client'
 import Link from 'next/link'
 import { Card, CardContent } from '../ui/card'
 import Image from 'next/image'
+import FavouriteToggleButton from './FavouriteToggleButton'
 
 function ProductsGrid(
   { products }: { products: Product[] }
@@ -31,9 +32,18 @@ function ProductsGrid(
                       className='rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500'
                     />
                   </div>
+                  <div className='mt-4 text-center'>
+                    <h2 className='text-lg uppercase'>{name}</h2>
+                    <p className='text-primary mt'>
+                      {poundsAmount}
+                    </p>
+                  </div>
                 </CardContent>
               </Card>
             </Link>
+            <div className='absolute top-7 right-7 z-5'>
+              <FavouriteToggleButton productId={productId} />
+            </div>
           </article>
         )
       })}   
