0c72f726579fd5bc718c5f1de4c226f32c49e3b6
Nimira43 <n.rabone72@aol.com>
2025-04-12 15:59:55 +0100

    ProductsGrid - part 1

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -1,7 +1,24 @@
-function ProductsGrid() {
+import { formatCurrency } from '@/utils/format'
+import { Product } from '@prisma/client'
+
+function ProductsGrid({
+  products
+}: { products: Product[] }) {
   return (
-    <div>
-      ProductsGrid      
+    <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      {products.map((product) => {
+        const { name, price, image } = product
+        const productId = product.id
+        const poundsAmount = formatCurrency(price)
+        return (
+          <article
+            key={productId}
+            className='group relative'>
+            Grid
+          </article>
+        )
+      })}   
+      
     </div>
   )
 }
