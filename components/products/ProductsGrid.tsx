658fd14da9c44319e8614037aa8a35880d51b1c6
Nimira43 <n.rabone72@aol.com>
2025-04-12 16:19:42 +0100

    ProductsGrid - 2. Updated next.config.mjs

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -1,9 +1,12 @@
 import { formatCurrency } from '@/utils/format'
 import { Product } from '@prisma/client'
+import Link from 'next/link'
+import { Card, CardContent } from '../ui/card'
+import Image from 'next/image'
 
-function ProductsGrid({
-  products
-}: { products: Product[] }) {
+function ProductsGrid(
+  { products }: { products: Product[] }
+) {
   return (
     <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
       {products.map((product) => {
@@ -14,7 +17,23 @@ function ProductsGrid({
           <article
             key={productId}
             className='group relative'>
-            Grid
+            <Link href={`/products/${productId}`}>
+              <Card
+                className='transform group-hover:shadow-xl transition-shadow duration-500'>
+                <CardContent className='p-4'>
+                  <div className='relative h-64 md:h-48 rounded overflow-hidden'>
+                    <Image
+                      src={image}
+                      alt={name}
+                      fill
+                      sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+                      priority
+                      className='rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500'
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
           </article>
         )
       })}   
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pexels.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 export default nextConfig
