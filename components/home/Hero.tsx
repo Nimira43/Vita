2e844e9b59575af2c67255d3cebc96ac99af10a0
Nimira43 <n.rabone72@aol.com>
2025-04-18 21:02:13 +0100

    Starting to work on Hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,8 +1,17 @@
+import HeroCarousel from "./HeroCarousel"
+
 function Hero() {
   return (
-    <div>
-      Hero      
-    </div>
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-2xl font-medium text-4xl tracking-tight sm:text-6xl'>
+          A Better Way to Shop Around
+        </h1>
+        <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>Discover a better way to shop at Vita—your one-stop destination for quality, style, and unbeatable convenience. Shop smart today!</p>
+      </div>
+      
+      <HeroCarousel />
+    </section>
   )
 }
 
