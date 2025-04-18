4803ca765e2cf6b6364675b5ece95facffd65fab
Nimira43 <n.rabone72@aol.com>
2025-04-18 22:23:31 +0100

    Tidied up Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import { Button } from "../ui/button"
-import HeroCarousel from "./HeroCarousel"
+import { Button } from '../ui/button'
+import HeroCarousel from './HeroCarousel'
 
 function Hero() {
   return (
