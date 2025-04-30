c146e79aeda884ec8955be2120a44eaaa7c29918
Nimira43 <n.rabone72@aol.com>
2025-04-30 17:28:54 +0100

    Importing images  into HeroCarousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,3 +1,9 @@
+import hero1 from '@/public/images/bed.jpg'
+import hero2 from '@/public/images/kitchen-table.jpg'
+import hero3 from '@/public/images/side-table.jpg'
+import hero4 from '@/public/images/sofa.jpg'
+
+
 function HeroCarousel() {
   return (
     <div>
