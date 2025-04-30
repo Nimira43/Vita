d1372a9108abc8c9bde2dc65fd02c9a43440f789
Nimira43 <n.rabone72@aol.com>
2025-04-30 17:33:41 +0100

    Starting to setup the carousel

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -2,13 +2,18 @@ import hero1 from '@/public/images/bed.jpg'
 import hero2 from '@/public/images/kitchen-table.jpg'
 import hero3 from '@/public/images/side-table.jpg'
 import hero4 from '@/public/images/sofa.jpg'
+import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from '../ui/carousel'
 
 const carouselImages = [hero1, hero2, hero3, hero4]
 
 function HeroCarousel() {
   return (
-    <div>
-      HeroCarousel      
+    <div className='hidden lg:block'>
+      <Carousel>
+        <CarouselContent></CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   )
 }
