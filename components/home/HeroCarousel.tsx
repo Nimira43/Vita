bd5212628385580fb34530edb3b83c62c3f9a5ec
Nimira43 <n.rabone72@aol.com>
2025-04-30 17:30:36 +0100

    Created carouselImages

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -3,6 +3,7 @@ import hero2 from '@/public/images/kitchen-table.jpg'
 import hero3 from '@/public/images/side-table.jpg'
 import hero4 from '@/public/images/sofa.jpg'
 
+const carouselImages = [hero1, hero2, hero3, hero4]
 
 function HeroCarousel() {
   return (
