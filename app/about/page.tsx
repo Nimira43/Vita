5a3290a8e8fd07dcb1df075f6701a6897f039861
Nimira43 <n.rabone72@aol.com>
2025-05-06 20:08:20 +0100

    About page - working of h1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,9 @@
 function AboutPage() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-medium leading-none trackiing-wide sm:text-6xl'>Shop with </h1>
+      <span>Vita</span>
+    </section>
   )
 }
 export default AboutPage
