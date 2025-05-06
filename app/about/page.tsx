48c587784e9c62401ebac97bf39f747f2b2b7af5
Nimira43 <n.rabone72@aol.com>
2025-05-06 20:14:59 +0100

    Logo styled.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,12 @@
 function AboutPage() {
   return (
     <section>
-      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-medium leading-none trackiing-wide sm:text-6xl'>Shop with </h1>
-      <span>Vita</span>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-light leading-none trackiing-wide sm:text-6xl'>Shop with 
+        <span className='text-primary logo'
+        >
+          Vita
+        </span>
+      </h1>
     </section>
   )
 }
