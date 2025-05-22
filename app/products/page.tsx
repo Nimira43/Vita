cd516314d0205699153a3ae9727bf41fbdc0a6b9
Nimira43 <n.rabone72@aol.com>
2025-05-23 00:32:12 +0100

    Products page completed for the moment.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -10,7 +10,10 @@ async function ProductsPage({
 
   return (
     <div>
-      <h1>Products Page</h1>
+      <ProductsContainer
+        layout={layout}
+        search={search}
+      />
     </div>
   )
 }
