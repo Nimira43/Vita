904468974a9ed14da69dda7200efa3d00ac2ec2a
Nimira43 <n.rabone72@aol.com>
2025-05-23 00:15:06 +0100

    LoadingProduct function

--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -9,4 +9,16 @@ function LoadingContainer() {
   )
 }
 
+function LoadingProduct() {
+  return (
+    <Card>
+      <CardContent className='p-4'>
+        <Skeleton className='h-48 w-full'/>
+        <Skeleton className='h-4 w-3/4 mt-4'/>
+        <Skeleton className='h-4 w-1/4 mt-4'/>
+      </CardContent>
+    </Card>
+  )
+}
+
 export default LoadingContainer
