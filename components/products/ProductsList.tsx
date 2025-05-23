fe5d7efc1b37abdaa4100a4d51ac2d883e0b2712
Nimira43 <n.rabone72@aol.com>
2025-05-23 01:25:05 +0100

    Styled the product image

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -18,7 +18,16 @@ function ProductsList({ products}: { products: Product[]}) {
             <Link href={`/products/${productId}`}>
               <Card className='transform group-hover:shadow-xl transition shadow duration-500'>
                 <CardContent className='p-8 gap-y-4 grid md:grid-cols-3'>
-                  
+                  <div className='relative h-64 md:h-48 md:w-48'>
+                    <Image
+                      src={image}
+                      alt={name}
+                      fill
+                      sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+                      priority
+                      className='w-full rounded-md object-cover'
+                    />
+                  </div>
                 </CardContent>
               </Card>
             </Link>
