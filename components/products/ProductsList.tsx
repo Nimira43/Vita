e6f91e6cea782d453ff2b4cae964d01605064cf2
Nimira43 <n.rabone72@aol.com>
2025-05-23 01:12:57 +0100

    Seting up variables.

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -1,9 +1,18 @@
+import { formatCurrency } from '@/utils/format'
 import { Product } from '@prisma/client'
 
 function ProductsList({ products}: { products: Product[]}) {
   return (
-    <div>
-      ProductsList      
+    <div className='mt-12 grid gap-y-8'>
+      {products.map((product) => {
+        const { name, price, image, company } = product
+        const poundsAmount = formatCurrency(price)
+        const productId = product.id
+
+        return (
+          
+        )
+      })}
     </div>
   )
 }
