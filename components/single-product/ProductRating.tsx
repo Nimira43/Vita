0dce48525e9e21a74ecb80851dbe02fea4431b07
Nimira43 <n.rabone72@aol.com>
2025-06-11 14:49:13 +0100

    Finished ProductRating.tsx for now.

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -4,10 +4,14 @@ async function ProductRating({productId}: {productId: string}) {
   const rating = 4.2
   const count = 25
 
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`
+  const countValue = `{${count}} reviews`
+
   return (
-    <div>
-      Product Rating      
-    </div>
+    <span className={className}>
+      <IoStar className='w-3 h-3' />
+      {rating} {countValue}      
+    </span>
   )
 }
 
