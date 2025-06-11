6bfc97f1b24505304feebbc58715a3a506821a62
Nimira43 <n.rabone72@aol.com>
2025-06-11 14:45:32 +0100

    Created hard-code variables for now.

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,6 +1,9 @@
 import { IoStar } from 'react-icons/io5'
 
 async function ProductRating({productId}: {productId: string}) {
+  const rating = 4.2
+  const count = 25
+
   return (
     <div>
       Product Rating      
