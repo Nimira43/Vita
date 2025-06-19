86b8ec128e480d2f57bff5517e1ae75b75c0c726
Nimira43 <n.rabone72@aol.com>
2025-06-19 09:14:00 +0100

    Setting up SingleProduct params and variables.

--- components/products/[id]/page.tsx
+++ components/products/[id]/page.tsx
@@ -1,8 +1,20 @@
-function page() {
+import BreadCrumbs from "@/components/single-product/BreadCrumbs"
+import { fetchSingleProduct } from "@/utils/actions"
+import { formatCurrency } from "@/utils/format"
+
+async function SingleProductPage({ 
+  params 
+}: {
+  params: { id: string } 
+ }) {
+  const product = await fetchSingleProduct(params.id)
+  const { name, image, company, description, price } = product
+  const poundsAmount = formatCurrency(price)
+  
   return (
-    <div>
-            
-    </div>
+    <section>
+      <BreadCrumbs name={product.name}/>
+    </section>
   )
 }
 
