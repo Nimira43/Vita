80d4ad0e7cb1f27d5d78995674194f3e46795fae
Nimira43 <n.rabone72@aol.com>
2025-07-01 21:35:21 +0100

    Setting up public routes.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/products(.*)',
+  '/about'
+])
+
+export default clerkMiddleware((auth, req) => {
+
+})
 
 export const config = {
   matcher: [
