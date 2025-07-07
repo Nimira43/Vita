f9bcafa0002b5d8af3d595461c45096f7fc9c6b8
Nimira43 <n.rabone72@aol.com>
2025-07-07 02:27:14 +0100

    DashboardLayout done for the moment.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,9 +5,18 @@ function DashboardLayout({ children}: {
   children: React.ReactNode
 }) {
   return (
-    <div>
-      Layout
-    </div>
+    <>
+      <h2 className='text-2xl pl-4'>Dashboard</h2>
+      <Separator className='mt-2' />
+      <section className='grid lg:grid-cols-12 gap-12 mt-12'>
+        <div className='lg:col-span-2'>
+          <Sidebar />
+        </div>
+        <div className='lg:col-span-10 px-4'>
+          {children}
+        </div>
+      </section>
+    </>
   )
 }
 
