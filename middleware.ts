1c07f09c66f10eb43931dd8da893064b89eb3448
Nimira43 <n.rabone72@aol.com>
2025-07-13 16:50:00 +0100

    Importing NextResponse inside middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/',
   '/products(.*)',
   '/about'
 ])
+
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) auth().protect()
