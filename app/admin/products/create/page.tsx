8e6f4af024ef08363f060c2607e963e567ff3759
Nimira43 <n.rabone72@aol.com>
2025-07-23 22:42:32 +0100

    createProductAction

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,7 +1,16 @@
+const createProductAction = async (formData: FormData) => {
+  'use server'
+  const name = formData.get('name') as string
+  console.log(name)
+}
+
 function CreateProductPage() {
   return (
     <section>
       <h1 className='text-2xl font-medium mb-8 uppercase'>Create Product</h1>
+      <div className='border p-8 rounded-md'>
+        <form action=''></form>
+      </div>
     </section>
   )
 }
