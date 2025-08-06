2314d63944c13d806c8c20425cb72d16490b7f49
Nimira43 <n.rabone72@aol.com>
2025-08-06 09:16:34 +0100

    CheckboxInput - 2

--- components/form/CheckBoxInput.tsx
+++ components/form/CheckBoxInput.tsx
@@ -1,5 +1,13 @@
+'use client'
+
 type CheckboxInputProps = {
   name: string
   label: string
   defaultChecked?: boolean
 }
+
+export default function CheckboxInput({
+  name,
+  label,
+  defaultChecked = false,
+}: CheckboxInputProps) {}
