bd8f94ff52fd9bacb39c63f686587a48bdc630a6
Nimira43 <n.rabone72@aol.com>
2025-08-06 09:49:48 +0100

    FormContainer - 3

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useToast } from '@/hooks/use-toast'
 import { actionFunction } from '@/utils/types'
 import React from 'react'
+import { useFormState } from 'react-dom'
 
 const initialState = {
   message: ''
@@ -13,6 +15,9 @@ function FormContainer({
 }: {
   action: actionFunction
   children: React.ReactNode
-}) {}
+}) {
+  const [state, formAction ] = useFormState(action, initialState)
+  const { toast } = useToast()
+}
 
 export default FormContainer
