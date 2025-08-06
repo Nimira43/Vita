9dc1bc901c83d8101634ecf22922972d7033a115
Nimira43 <n.rabone72@aol.com>
2025-08-06 09:04:14 +0100

    ImageInput - 2

--- components/form/ImageInput.tsx
+++ components/form/ImageInput.tsx
@@ -1,5 +1,18 @@
-function ImageInput() {
+import { Label } from "../ui/label"
 
+function ImageInput() {
+  const name = 'image'
+
+  return (
+    <div className='mb-2'>
+      <Label
+        htmlFor={name}
+        className='uppercase'
+      >
+        Image
+      </Label>
+    </div>
+  )
 }
 
 export default ImageInput
