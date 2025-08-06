8330c98110220935fa6f7f1c95f2ec81fc3934a8
Nimira43 <n.rabone72@aol.com>
2025-08-06 09:47:56 +0100

    FormContainer - 2

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,6 +1,18 @@
 'use client'
 
+import { actionFunction } from '@/utils/types'
+import React from 'react'
+
 const initialState = {
   message: ''
 }
 
+function FormContainer({
+  action,
+  children,
+}: {
+  action: actionFunction
+  children: React.ReactNode
+}) {}
+
+export default FormContainer
