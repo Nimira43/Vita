265e941a6671e18b329e28c5fb19c6488769c706
Nimira43 <n.rabone72@aol.com>
2025-08-06 08:59:25 +0100

    PriceInput - 3

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -1,3 +1,5 @@
+import { Label } from "../ui/label"
+
 const name = 'price'
 type FormInputNumberProps = {
   defaultValue?: number
@@ -5,4 +7,18 @@ type FormInputNumberProps = {
 
 function PriceInput(
   { defaultValue }: FormInputNumberProps
-) {}
+) {
+  return (
+    <div className='mb-2'>
+      <Label
+        htmlFor='price'
+        className='uppercase'
+      >
+        Price (£)
+      </Label>
+    </div>
+  )
+
+}
+
+export default PriceInput
