890684d0d1d276dfa0f7cc2e4a8b0f9c46b0d850
Nimira43 <n.rabone72@aol.com>
2025-08-14 12:37:21 +0100

    Deleted old form

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -13,22 +13,12 @@ function CreateProduct() {
   const name = faker.commerce.productName()
   const company = faker.company.name()
   const description = faker.lorem.paragraph({ min: 10, max: 12})
+
   return (
     <section>
       <h1 className='text-2xl font-medium mb-8 uppercase'>Create Product</h1>
       <div className='border p-8 rounded-md'>
-        <form action={createProductAction}>
-          <div className='mb-2'>
-            <Label htmlFor='name'>Product Name</Label>
-            <Input 
-              id='name' 
-              name='name' 
-              type='text' 
-              defaultValue={name}
-            />
-          </div>
-          <Button type='submit' size='lg'>Submit</Button>
-        </form>
+      
       </div>
     </section>
   )
