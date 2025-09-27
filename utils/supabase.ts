a7db76c2987ba3b9498b373ab3142df3a6228d52
Nimira43 <n.rabone72@aol.com>
2025-09-27 19:33:27 +0100

    supabase createClient

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,2 +1,8 @@
 import { createClient} from '@supabase/supabase-js'
 
+const bucket = 'main-bucket'
+
+export const supabase = createClient(
+  process.env.SUPERBASE_URL as string,
+  process.env.SUPERBASE_KEY as string
+)
