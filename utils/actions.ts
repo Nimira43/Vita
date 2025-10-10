ec0ae2d588a7df880576749313ef9b209f68ac4d
Nimira43 <n.rabone72@aol.com>
2025-10-10 20:32:44 +0100

    getAdminUser - 2

--- utils/actions.ts
+++ utils/actions.ts
@@ -16,6 +16,8 @@ const getAuthUser = async () => {
 
 const getAdminUser = async () => {
   const user = await getAuthUser()
+  if (user.id !== process.env.ADMIN_USER_ID) redirect('/')
+  return user
 }
 
 const renderError = (error: unknown): { message: string } => {
