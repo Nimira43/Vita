b3b955d461daa504b2ffd1b22d5d09a5005a4a04
Nimira43 <n.rabone72@aol.com>
2025-11-23 19:56:48 +0000

    EditProductPage - 2

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,9 +1,13 @@
+import FormContainer from '@/components/form/FormContainer'
+
 function EditProductPage() {
   return (
     <section>
       <h1 className='text-2xl font-medium mb-8 uppercase'>Update Product</h1>
       <div className='border p-8 rounded-md'>
-        
+        <FormContainer action=''>
+          
+        </FormContainer>
       </div>
     </section>
   )
