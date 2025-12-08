8fbc10d62723ee7eb66a6a761de735ff8c2f2eb3
Nimira43 <n.rabone72@aol.com>
2025-12-08 22:57:58 +0000

    Adding type to import of actionFunction

--- components/form/ImageInputContainer.tsx
+++ components/form/ImageInputContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { actionFunction } from '@/utils/types'
+import { type actionFunction } from '@/utils/types'
 import Image from 'next/image';
 import { useState } from 'react';
 import { Button } from '../ui/button';
