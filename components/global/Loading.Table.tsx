8e5e2f2af449b4cccf4b8c17749f7ddd0f205668
Nimira43 <n.rabone72@aol.com>
2025-12-09 00:06:26 +0000

    LoadingTable - 2

--- components/global/Loading.Table.tsx
+++ components/global/Loading.Table.tsx
@@ -1,6 +1,10 @@
 import { Skeleton } from '../ui/skeleton'
 
-function LoadingTable({ row = 5 }) {
+function LoadingTable({
+  row = 5
+}: {
+  rows?: number
+}) {
   return (
     <div className='mb-4 key={index}'>
       <Skeleton className='/>
