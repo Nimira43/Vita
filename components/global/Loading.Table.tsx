a1156a8c805720c1250549e8bd182964a3665e8a
Nimira43 <n.rabone72@aol.com>
2025-12-09 00:09:45 +0000

    LoadingTable - 3

--- components/global/Loading.Table.tsx
+++ components/global/Loading.Table.tsx
@@ -1,15 +1,18 @@
 import { Skeleton } from '../ui/skeleton'
 
 function LoadingTable({
-  row = 5
+  rows = 5
 }: {
   rows?: number
 }) {
-  return (
-    <div className='mb-4 key={index}'>
-      <Skeleton className='/>
-    </div>
-  )
+  const tableRows = Array.from({ length: rows }, (_, index) => {
+    return (
+      <div className='mb-4 key={index}'>
+        <Skeleton className='' />
+      </div>
+    )
+  })  
+  return <>{tableRows}</> 
 }
 
 export default LoadingTable
