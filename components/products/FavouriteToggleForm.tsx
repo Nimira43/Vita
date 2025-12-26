e9a216d1a38cf8932d3cbf1749c9fbafbfcdf6ba
Nimira43 <n.rabone72@aol.com>
2025-12-26 19:16:00 +0000

    FavouriteToggleFormProps type

--- components/products/FavouriteToggleForm.tsx
+++ components/products/FavouriteToggleForm.tsx
@@ -3,6 +3,8 @@ import FormContainer from '../form/FormContainer'
 import { toggleFavouriteAction } from '@/utils/actions'
 import { CardSubmitButton } from '../form/Buttons'
 
+type FavouriteToggleFormProps = {}
+
 function FavouriteToggleForm() {
   return (
     <div>
