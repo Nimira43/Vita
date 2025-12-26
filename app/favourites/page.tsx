98b8817268d26c74b1f40fe4d0bc1c0f279ccdff
Nimira43 <n.rabone72@aol.com>
2025-12-26 20:11:24 +0000

    Importing ProductGrid

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,5 +1,6 @@
 import { fetchUserFavourites } from '@/utils/actions'
 import SectionTitle from '@/components/global/SectionTitle'
+import ProductsGrid from '@/components/products/ProductsGrid'
 
 function FavouritePage() {
   return (
