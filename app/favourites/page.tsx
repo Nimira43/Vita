e043ceebff7e9ea7bcaa3dbf1a7ed5906bae0717
Nimira43 <n.rabone72@aol.com>
2025-12-26 20:55:50 +0000

    Checking for favourites

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -4,6 +4,15 @@ import ProductsGrid from '@/components/products/ProductsGrid'
 
 async function FavouritePage() {
   const favourites = await fetchUserFavourites()
+
+  if (favourites.length === 0) {
+    return (
+      <SectionTitle 
+        text='You have no favourites yet.'
+      />
+    )
+  }
+
   return (
     <div>
       <h1>Favourite Page</h1>
