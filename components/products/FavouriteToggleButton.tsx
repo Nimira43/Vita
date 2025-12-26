5c6e571e4d46933ba707f591d7f626a4708296b8
Nimira43 <n.rabone72@aol.com>
2025-12-26 19:03:58 +0000

    Importing CardSignInButton

--- components/products/FavouriteToggleButton.tsx
+++ components/products/FavouriteToggleButton.tsx
@@ -1,6 +1,7 @@
 import { RiHeartLine } from 'react-icons/ri'
 import { Button } from '@/components/ui/button'
 import { auth } from '@clerk/nextjs/server'
+import { CardSignInButton } from '../form/Buttons'
 
 function FavouriteToggleButton({ productId }:{productId: string}) {
   return (
