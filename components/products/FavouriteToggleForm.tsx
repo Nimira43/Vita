9b2d2073f7b7498dce30041cca4dcedfaff39a34
Nimira43 <n.rabone72@aol.com>
2025-12-26 19:14:34 +0000

    Importing toggleFavouriteAction

--- components/products/FavouriteToggleForm.tsx
+++ components/products/FavouriteToggleForm.tsx
@@ -1,5 +1,6 @@
 import { usePathname } from 'next/navigation'
 import FormContainer from '../form/FormContainer'
+import { toggleFavouriteAction } from '@/utils/actions'
 
 function FavouriteToggleForm() {
   return (
