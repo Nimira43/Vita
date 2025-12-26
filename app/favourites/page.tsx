407fae8b32dd882eec0d7f040d5bbaab3cfb5fcb
Nimira43 <n.rabone72@aol.com>
2025-12-26 20:53:34 +0000

    favourites

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -2,7 +2,8 @@ import { fetchUserFavourites } from '@/utils/actions'
 import SectionTitle from '@/components/global/SectionTitle'
 import ProductsGrid from '@/components/products/ProductsGrid'
 
-function FavouritePage() {
+async function FavouritePage() {
+  const favourites = await fetchUserFavourites()
   return (
     <div>
       <h1>Favourite Page</h1>
